844cc56cbc680f28b26a1d2a6de58a4efe631138
Alexis Francois <francoisalexis.pro@gmail.com>
2021-03-04 11:22:38 +0100

    basic map fitted on pins ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,6 +26,7 @@ require("channels")
 import "bootstrap";
 
 import { initFlatpickr } from "../plugins/flatpickr";
+import { initMapbox } from '../plugins/init_mapbox';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -34,4 +35,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initFlatpickr();
+  initMapbox();
 });
+
+
